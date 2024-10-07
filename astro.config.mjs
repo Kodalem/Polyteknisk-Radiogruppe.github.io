24af3432747bc11a029dd770d42eb5871a77526c
Christine Elisabeth Koppel <kuutruu@posteo.net>
2024-10-07 17:23:13 +0200

    More stuff added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,29 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import mdx from '@astrojs/mdx';
+import mdxRenderer from "@astrojs/mdx/server.js";
+import rehypeKatex from 'rehype-katex';
+import remarkMdx from "remark-mdx";
+//import remarkGithub from "remark-github"; THIS BORKS THE BUILD
+import remarkImages from "remark-images";
+import codeImport from 'remark-code-import';
+
+import react from '@astrojs/react';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [mdx(
+      {
+        remarkPlugins: [rehypeKatex, remarkMdx, remarkImages, codeImport],
+      }
+  ), react()],
+  markdown: {
+    shikiConfig: {
+      themes:{
+        light: 'catppuccin-latte',
+        dark: 'catppuccin-mocha',
+      },
+    },
+  },
+});
---
+++ src/pages/projects/project_list_dir/stashescliopboard.md
@@ -0,0 +1,12 @@
+---
+layout: '../../../layouts/MarkdownProjectLayout.astro'
+title: 'Website Overhaul'
+pubDate: 2024-10-07
+description: 'Overhaul for the current website, with newer design and features'
+team_lead: ["Christine Elisabeth Koppel"]
+tags: ["software", "server", "webdev"]
+---
+Settings
+
+```astro file=<rootDir>/src/layouts/MarkdownProjectLayout.astro
+```
